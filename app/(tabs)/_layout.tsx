8c1e9c47994bc6535775483cf0f34c5ad74da38e
marcosherna <marcosxdf6@gmail.com>
2025-08-28 10:33:04 -0600

    feat: add icons tab bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,45 @@
 import { Tabs } from "expo-router";
+import MIcon from "@expo/vector-icons/MaterialIcons";
 
 export default function TabLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="home/index" options={{ title: "Home" }} />
-      <Tabs.Screen name="search/index" options={{ title: "Search" }} />
-      <Tabs.Screen name="library/index" options={{ title: "Library" }} />
-      <Tabs.Screen name="profile/index" options={{ title: "Profile" }} />
+      <Tabs.Screen
+        name="home/index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <MIcon name="home" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search/index"
+        options={{
+          title: "Search",
+          tabBarIcon: ({ color }) => (
+            <MIcon name="search" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="library/index"
+        options={{
+          title: "Library",
+          tabBarIcon: ({ color }) => (
+            <MIcon name="library-music" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile/index"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color }) => (
+            <MIcon name="person" size={24} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
